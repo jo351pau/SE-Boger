c3c0a8ece7d6691f234dfa7b20adac7408b4dd97
Lars <schlageterlars@gmail.com>
2023-10-15 15:12:07 +0200

    feat: set pieces on fields

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,15 +1,46 @@
-class Game(val player1: List[Row], val player2: List[Row]){
+class Game(val fields: List[Field]) {
 
-    def this(numberOfFields: Int) = this(List.fill(numberOfFields)(new Row()), List.fill(numberOfFields)(new Row()))
+  def this(numberOfFields: Int, numberOfPieces: Int) = {
+    this(Game.createFieldsWithInitialSetup(numberOfFields, numberOfPieces))
+  }
 
-    def copy(newPlayer1: List[Row], newPlayer2: List[Row]): Game = new Game(newPlayer1, newPlayer2)
+  override def toString: String = fields.mkString("  ")
 
-    override def toString: String = {
-        val output1 = player1.mkString("\t")
-        val output2 = player2.mkString("\t")
+}
 
-        val midline = (0 until player1.length).map(_ => "-").mkString("\t")
+private object Game {
+  def createFieldsWithInitialSetup(
+      fields: Int,
+      pieces: Int
+  ): List[Field] = {
 
-        return output1 + "\n" + midline + "\n" + output2
+    def createField(index: Int): Field = {
+      def quarter(q: Int) = ((fields / 4) * q)
+
+      val number = index match {
+        case 0 => NumberOfPieces.Begin.value(pieces)
+        case secondQ if secondQ == quarter(1) =>
+          -NumberOfPieces.Begin.value(pieces)
+        case endTwo if endTwo == quarter(1) - 2 =>
+          -NumberOfPieces.End.value(pieces)
+        case midTwo if midTwo == quarter(2) - 1 =>
+          NumberOfPieces.Mid.value(pieces)
+        case _: Int => 0
+      }
+      Field(number)
     }
+    val side = List.tabulate(fields / 2)(index => createField(index))
+    side ++ side.map(f => Field(-f.pieces)).reverse
+  }
+
+  private def remaining(n: Int) = n - (NumberOfPieces.Begin.value(n) * 2)
+  private enum NumberOfPieces(calculate: Int => Int) {
+    def value(number: Int) = calculate(number)
+
+    case Begin extends NumberOfPieces((n: Int) => (n / 3).toInt)
+    case Mid extends NumberOfPieces((n: Int) => remaining(n) / 2)
+    case EndQ2 extends NumberOfPieces((n: Int) => (n / 3).toInt)
+    case End
+        extends NumberOfPieces((n: Int) => remaining(n) - (remaining(n) / 2))
+  }
 }
