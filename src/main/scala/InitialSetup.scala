e747bbf4dd8370ce069d3d655343380488115837
Lars <schlageterlars@gmail.com>
2023-10-17 15:07:37 +0200

    refactor: removed parameter to calculate pieces

--- src/main/scala/InitialSetup.scala
+++ src/main/scala/InitialSetup.scala
@@ -7,15 +7,15 @@ class DefaultInitialSetup(fields: Int, pieces: Int)
     extends InitialSetup(fields, pieces) {
   override def get: Map[Int, Int] =
     Map(
-      0 -> NumberOfPieces.Most.value(pieces),
-      quarter(1) -> -NumberOfPieces.Most.value(pieces),
-      quarter(1) - 2 -> -NumberOfPieces.End.value(pieces),
-      quarter(2) - 1 -> -NumberOfPieces.Mid.value(pieces)
+      0 -> NumberOfPieces.Most.value,
+      quarter(1) -> -NumberOfPieces.Most.value,
+      quarter(1) - 2 -> -NumberOfPieces.End.value,
+      quarter(2) - 1 -> -NumberOfPieces.Mid.value
     )
 
-  private def remaining(n: Int) = n - (NumberOfPieces.Most.value(n) * 2)
+  private def remaining(n: Int) = n - (NumberOfPieces.Most.value * 2)
   private enum NumberOfPieces(calculate: Int => Int) {
-    def value(number: Int) = calculate(number)
+    def value = calculate(pieces)
 
     case Most extends NumberOfPieces((n: Int) => (n / 3).toInt)
     case Mid extends NumberOfPieces((n: Int) => remaining(n) / 2)
