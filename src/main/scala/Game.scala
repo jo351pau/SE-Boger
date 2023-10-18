c4d5c3401cd59cfa3f7e0f8e354f8c19e66ee05d
Lars <schlageterlars@gmail.com>
2023-10-18 18:44:31 +0200

    feat: build structure

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,16 +1,23 @@
 class Game(val fields: List[Field]) {
 
   def this(fields: Int, pieces: Int) =
-    this(Game.createFields(DefaultInitialSetup(fields, pieces)))
+    this(Game.create(DefaultInitialSetup(fields, pieces)))
 
-  def this(setup: InitialSetup) = this(Game.createFields(setup))
+  def this(setup: InitialSetup) = this(Game.create(setup))
+
+  def copy(from: Int, to: Int, pieces: Int) : Game = {
+    //TODO: Implement logic to move pieces
+    Game(fields)
+  }
+
+  def winner : Option[Player] = Option.empty // TODO
 
   override def toString: String = fields.mkString("  ")
 
 }
 
 private object Game {
-  def createFields(
+  def create(
       setup: InitialSetup
   ): List[Field] = {
     val side = List.tabulate(setup.fields / 2)(index =>
@@ -24,4 +31,9 @@ private object Game {
     )
     side ++ side.map(f => Field(-f.pieces)).reverse
   }
+}
+
+enum Player {
+  case One
+  case Two
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,21 @@
-val numberOfFields = 12
-val numberOfFigures = 1
-val game: Game = Game (numberOfFields)
+val fields = 24
+val numberOfPieces = 15
+val game: Game = Game(fields, numberOfPieces)
 
-@main def run() : Unit = {
+@main def run(): Unit = {
   println(game)
+  // TODO: call startNewRound
 }
 
-
-
-
-
+def startNewRound(game: Game): Game = {
+  // TODO: roll the dice,
+  // move pieces
+  game.winner
+    .map(value => {
+      // TODO: finish game
+      game
+    })
+    .getOrElse {
+      startNewRound(game)
+    }
+}
