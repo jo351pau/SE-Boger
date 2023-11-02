0f4e1252683afc18e78e1b640f5620fdb8fdc442
Lars <schlageterlars@gmail.com>
2023-11-02 12:08:04 +0100

    feat: logic to move pieces

--- src/main/scala/Field.scala
+++ src/main/scala/Field.scala
@@ -1,13 +1,29 @@
 class Field(val pieces: Int) {
 
   def this() = this(0)
-  
+
   override def equals(field: Any): Boolean = field.match {
     case f: Field => f.pieces == pieces
-    case i: Int => i == pieces
+    case i: Int   => i == pieces
     case _        => false
   }
 
   override def toString: String =
     if (pieces != 0) s"|$pieces|" else "|/|"
+
+  def +(that: Int): Field = Field(pieces + that)
+
+  def -(that: Int): Field = Field(pieces - that)
+
+  def toPositive() = if (pieces < 0) -pieces else pieces
+
+  def isNegative() = pieces < 0
+
+  def isOccupied() = pieces != 0
+
+  def isEmpty() = pieces == 0
+
+  def hasSameSignAs(that: Field) = this.pieces * that.pieces < 0
+
+  def hasEnoughPieces(pieces: Int) = toPositive() - pieces >= 0
 }
--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,3 +1,5 @@
+import scala.util.Random
+
 class Game(val fields: List[Field]) {
 
   def this(fields: Int, pieces: Int) =
@@ -6,13 +8,39 @@ class Game(val fields: List[Field]) {
   def this(setup: Setup) = this(Game.create(setup))
 
   def copy(from: Int, to: Int, pieces: Int): Game = {
-    // TODO: Implement logic to move pieces
-    Game(fields)
+    if fields(from).isEmpty() then
+      println(s"Not possible! Field $from is empty.")
+    else if !(fields(from) hasEnoughPieces pieces) then
+      println(
+        s"Not possible! ${fields(from).toPositive()} pieces on field $from, tried to move $pieces."
+      )
+    else if fields(to).isOccupied() && (fields(from) hasSameSignAs fields(to))
+    then println(s"Not possible! Different players on fields $from and $to.")
+    else
+      val value = if (fields(from).isNegative()) -pieces else pieces
+      return copy(
+        Map(
+          from -> (fields(from) - value),
+          to -> (fields(to) + value)
+        )
+      )
+    this
   }
+
+  private def copy(changes: Map[Int, Field]): Game =
+    Game(
+      List.tabulate(fields.length)(index =>
+        changes.getOrElse(index, fields(index))
+      )
+    )
 
   def winner: Option[Player] = Option.empty // TODO
 
   override def toString: String = fields.mkString(" ")
+
+  def ==(that: Game): Boolean = fields.equals(that.fields)
+
+  def !=(that: Game): Boolean = !(this == that)
 
 }
 
@@ -21,16 +49,12 @@ private object Game {
       setup: Setup
   ): List[Field] = {
     val side = List.tabulate(setup.fields / 2)(index =>
-      Field(
-        setup.get
-          .collectFirst {
-            case (position, pieces) if position == index => pieces
-          }
-          .getOrElse(0)
-      )
+      Field(setup.toMap.getOrElse(index, 0))
     )
     side ++ side.map(f => Field(-f.pieces)).reverse
   }
+
+  def rollDice(): Int = Random.nextInt(6) + 1
 }
 
 enum Player {
