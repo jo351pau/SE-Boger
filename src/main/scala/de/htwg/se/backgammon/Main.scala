0d237e3c640b742e12922bd5e0db00ef40a03860
smooth use <smoothuse@gmail.com>
2023-12-13 13:06:58 +0100

    feat: start tui and gui parallel

--- src/main/scala/de/htwg/se/backgammon/Main.scala
+++ src/main/scala/de/htwg/se/backgammon/Main.scala
@@ -4,13 +4,27 @@ import de.htwg.se.backgammon.controller.Controller
 import de.htwg.se.backgammon.view.TUI
 import de.htwg.se.backgammon.model.Model
 import de.htwg.se.backgammon.model.Game
+import de.htwg.se.backgammon.model.CustomSetup
+import de.htwg.se.backgammon.view.GUI
+import scalafx.application.JFXApp3
+import scala.concurrent.Future
+import scala.concurrent.Await
 
 private val NUMBER_OF_FIELDS = 24
 private val NUMBER_OF_FIGURES = 15
 
-@main def run(): Unit = {
-  val game = new Game(NUMBER_OF_FIELDS, NUMBER_OF_FIGURES)
-  val controller = Controller(Model(game))
-  val tui = TUI(controller)
-  tui.run
+object Main {
+  val controller = new Controller(new Game(NUMBER_OF_FIELDS, NUMBER_OF_FIGURES))
+  val tui = new TUI(controller)
+  val gui = new GUI(controller)
+
+  @main def run(): Unit = {
+    implicit val context = scala.concurrent.ExecutionContext.global
+    val f = Future {
+      gui.main(Array[String]())
+    }
+    tui.run
+    Await.ready(f, scala.concurrent.duration.Duration.Inf)
+  }
+
 }
